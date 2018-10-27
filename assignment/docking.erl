b3878ba2580100363e947354fc2ae0bc59f1576e
Xiao Liu <liux0047@gmail.com>
2018-10-27 12:25:29 +0100

    empty and idle state works

--- assignment/docking.erl
+++ assignment/docking.erl
@@ -4,42 +4,45 @@
 -export([start_link/3]).
 -export([
     init/1, callback_mode/0, terminate/3,
-    release_moped/1,
+    release_moped/1, secure_moped/1,
     empty/3, idle/3
 ]).
 
 start_link(Total, Occupied, Name) ->
     gen_statem:start_link({local,Name}, ?MODULE, {Total, Occupied}, []).
 
+init({Total, 0}) ->
+    {ok, empty, {Total, 0}};
 init({Total, Occupied}) ->
-    {ok, empty, {Total, Occupied}}. 
+    {ok, idle, {Total, Occupied}}. 
 
-empty({call, {From, _}}, release_moped, Data) ->
-    io:format("empty state: keep_state_and_data from ~p", [From]),
-    From ! {error, empty},
+
+empty({call, From}, release_moped, {_, 0}) ->
+    io:format("empty state | release: keep_state_and_data from ~p~n", [From]),
+    gen_statem:reply(From, {error, empty}),
     keep_state_and_data;
-empty({call, {From, _}}, secure_moped, Data) ->
-    {next_state, idle, Data}.
+empty({call, From}, secure_moped, {Total, 0}) ->
+    io:format("empty state | secure: next_state=idle from ~p~n", [From]),
+    gen_statem:reply(From, ok),
+    {next_state, idle, {Total, 1}}.
 
-idle({call, _}, release_moped, Data) ->
-    ok.
+idle({call, From}, release_moped, {Total, 1}) ->
+    io:format("idle state | release: Total, 1~n",[]),
+    gen_statem:reply(From, ok),
+    {next_state, empty, {Total, 0}};
+idle({call, From}, release_moped, {Total, Occupied}) ->
+    io:format("idle state | release: Total, Occupied = ~p~n",[Occupied]),
+    gen_statem:reply(From, ok),
+    {keep_state, {Total, Occupied -1}}.
 
 full() ->
     ok.
 
 release_moped(Name) ->
-    gen_statem:call(Name, release_moped),
-    receive
-        {error, empty} ->
-            io:format("received {error, empty}"),
-            {error, empty};
-        _ ->
-            ok
-    end.
+    gen_statem:call(Name, release_moped).
 
 secure_moped(Name) ->
-    gen_statem:call(Name, secure_moped),
-    ok.
+    gen_statem:call(Name, secure_moped).
 
 callback_mode() ->
     state_functions.
--- assignment/test.erl
+++ assignment/test.erl
@@ -3,4 +3,5 @@
 
 part1() ->
     io:format("~w~n", [docking:start_link(3,0, kellogg)]),
-    io:format("~w~n", [docking:release_moped(kellogg)]).
+    io:format("~w~n", [docking:release_moped(kellogg)]),
+    io:format("~w~n", [docking:secure_moped(kellogg)]).
